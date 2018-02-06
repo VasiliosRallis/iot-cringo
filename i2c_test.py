f0ca6e459a1254e3413d37a3e829c6be627badf4
christmas <Christmas@dyn1172-221.wlan.ic.ac.uk>
2018-02-06 19:08:34 +0000

    connected to the network setup broker for communicating data to the server
    in-progress:
    	setup callback function and receive data back from the server
    	adjust thresholds
    	reset game on long press

--- i2c_test.py
+++ i2c_test.py
@@ -11,78 +11,129 @@ import array
 import ustruct as struct
 import urandom as random
 from machine import I2C, Pin
+import ujson as json
+from umqtt.simple import MQTTClient
+import network
 
-START_THR = 15000
+RESET_THR = 15000
 NEXT_THR = 3000
 N = 7
 
 SLAVEADDR = 19
 
 COMMANDREG = 0x80
-PROXIMITYRATE = 0x82
-LIGHTSENSORRATE = 0x84
+PROXIMITYPARAMETERS = 0x82
+LIGHTPARAMETERS = 0x84
 LIGHTREG = 0x85
 PROXREG = 0x87
 
 def main():
+    print('Connecting to network...')
+    initNetwork()
+
+    client = setupClient()
+
     Bingo = False
     #specify the pins for the i2c communnication with the peripherals and fequency used
     i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
-    # wait until the user touches the proximity sensor to start the game
-    bootstrap(i2c)
-    print('Game has started')
-    # get the seed for the random sequence
-    light = read_ambient_on_demand(i2c)
-    # start a random sequence
+    
+    # wait until the user touches the proximity sensor
+    # get the seed to start the random sequence
+    light = bootstrap(i2c)
     random.seed(light)
-    # access command register, enable self-timed measurements(bit 0), periodic proximity (bit 1) 
-    i2c.writeto_mem(SLAVEADDR,COMMANDREG,b'\x03')
-    # keep 
+    print('Seed: ' + str(light))
 
+    # Start game
+    print('Game ON')
     results = results_init()
-
+    ctr = 0
     while Bingo != True:
         proximity = read_prox(i2c)
-        if (proximity > NEXT_THR): 
+        if (proximity > NEXT_THR):
+        #################################################### 
+        # check from the server if there is a bingo
+        client.check_msg()
+        ####################################################  
             while True:
                 # generate a new random number
                 r = random.getrandbits(N)
-                sample = int(r * pow(2,N-1)/(90-1))
+                sample = int(r * pow(2,N-1)/90)
 
-                if (results[sample] != 0) || (sample == 0):
-                    print(str(sample) + 'Found again') # generate another random number
+                # if ((results[sample] != 0) || (sample == 0)):
+                if sample == 0 or sample > 90:
+                    #ignore reading
+                    print(str(sample) + ' is ignored. Generating another number')
+                elif results[sample-1] != 0: 
+                    print('Number ' + str(sample) + ' spotted again. Generating another number') # generate another random number
                 else:
-                    results[sample] = sample
-                    print(results[sample])
+                    results[sample-1] = sample    
+                    ctr = ctr + 1
+                    print('Ctr: ' + str(ctr) + ' Sample: ' + str(results[sample-1]))
+                    json_string = {'Seed': str(light), 'Counter': str(ctr), 'Sample': str(sample)}
+                    send_data(client,json_string)
                     break
+
+            if ctr == 90:
+                Bingo = True
 ##################### FILL IN ##################### 
             # pass it forward to the network
             # wait for some time to avoid same package sent multiplie times
             time.sleep(1)
 #################################################### 
+def setupClient():
+    client = MQTTClient(machine.unique_id(), '192.168.0.10') #New MQTT instance
+    client.connect()
+    # client.set_callback(callback_function()) #Callback function for reading from MQTT
+    # client.subscribe("esys/cringo/results")
+
+    return client
+
+def initNetwork():
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+    sta_if =network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('EEERover','exhibition')
+    #Keep trying until it connects to the network
+    while not sta_if.isconnected():
+        pass
+    print('The connection was successfull')
+
+def send_data(client,json_string): 
+    data= json.dumps(json_string)
+    client.publish('esys/cringo/samples', bytes(data, 'utf-8'))#send  data to broker
+
 def results_init():
     lst = []
-    for i in range(1,91):
+    for i in range(0,90):
         lst.append(0)
     return lst
 
 def bootstrap(i2c):
     start = False
-    # access command register, enable self-timed measurements(bit 0), periodic proximity (bit 1) 
-    i2c.writeto_mem(SLAVEADDR,COMMANDREG,b'\x03')
+    # access command register, enable self-timed measurements(bit 0), periodic proximity (bit 1) and ambient light (bit 2) 
+    i2c.writeto_mem(SLAVEADDR,COMMANDREG,b'\x07')
     # access proximity rate register, increase rate to 30 samples/s
-    i2c.writeto_mem(SLAVEADDR,PROXIMITYRATE,b'\x04')
+    i2c.writeto_mem(SLAVEADDR,PROXIMITYPARAMETERS,b'\x04')
+    # set ambient light sensor to continuous conversion mode taking 10 samples/s and averaging them
+    i2c.writeto_mem(SLAVEADDR,LIGHTPARAMETERS,b'\xF7')
+    
     # take readings until someone touches the proximity sensor
     while start!=True:
         proximity = read_prox(i2c)
-        print(proximity)
-        if proximity > START_THR:
+        # print(proximity)
+        if proximity > NEXT_THR:
+            print('Starting game....')
             start = True
+    
+    time.sleep(1)
+    light = read_ambient(i2c)
+    # disable ambient light (bit 2) sensor
+    i2c.writeto_mem(SLAVEADDR,COMMANDREG,b'\x03')
+    return light
 
-def read_ambient_on_demand(i2c):
+def read_ambient(i2c):
     # request an ambient light measurement
-    time.sleep(0.1)
-    i2c.writeto_mem(SLAVEADDR,COMMANDREG,b'\x10')
     data = i2c.readfrom_mem(SLAVEADDR,LIGHTREG,2)
     conv_data = convert(data)
     return conv_data
