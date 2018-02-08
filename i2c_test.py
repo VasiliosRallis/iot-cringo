8b941a8addebf41118f1a1802757ad54f5d3771d
christmas <Christmas@dyn1172-132.wlan.ic.ac.uk>
2018-02-08 15:54:41 +0000

    first drafted code - communicates with sensors - generates random sequences with different seed - connects to the network - communicates via MQTT both publishes and subscribes - game ends on BINGO from one or more players at the same time
    on-going:
    - add comments
    - restart the game on demand with different seed
    - calibrate the thresholds
    - look for optimisations

--- i2c_test.py
+++ i2c_test.py
@@ -1,8 +1,9 @@
-""" need to:
-        - adjust the thresholds
-        - connect to internet
-        - reconsider when to take measurements (consider the timings too)
-        - change the rate of reading for the ambient light value and average
+"""
+on-going:
+- add comments
+- restart the game on demand with different seed
+- calibrate the thresholds
+- look for optimisations
 """
 import machine
 import time
@@ -16,8 +17,8 @@ from umqtt.simple import MQTTClient
 import network
 
 RESET_THR = 15000
-NEXT_THR = 3000
-N = 7
+NEXT_THR = 7000
+N = 7       # number of bits
 
 SLAVEADDR = 19
 
@@ -27,15 +28,14 @@ LIGHTPARAMETERS = 0x84
 LIGHTREG = 0x85
 PROXREG = 0x87
 
+BINGO = False
+
 def main():
     print('Connecting to network...')
     initNetwork()
-
-    client = setupClient()
-
-    Bingo = False
     #specify the pins for the i2c communnication with the peripherals and fequency used
     i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+    client = setupClient()
     
     # wait until the user touches the proximity sensor
     # get the seed to start the random sequence
@@ -45,46 +45,50 @@ def main():
 
     # Start game
     print('Game ON')
+    global BINGO
     results = results_init()
     ctr = 0
-    while Bingo != True:
+
+    while BINGO != True:
         proximity = read_prox(i2c)
         if (proximity > NEXT_THR):
-        #################################################### 
+
         # check from the server if there is a bingo
-        client.check_msg()
-        ####################################################  
-            while True:
-                # generate a new random number
-                r = random.getrandbits(N)
-                sample = int(r * pow(2,N-1)/90)
+            client.check_msg()
+            print(BINGO)
 
-                # if ((results[sample] != 0) || (sample == 0)):
-                if sample == 0 or sample > 90:
-                    #ignore reading
-                    print(str(sample) + ' is ignored. Generating another number')
-                elif results[sample-1] != 0: 
-                    print('Number ' + str(sample) + ' spotted again. Generating another number') # generate another random number
-                else:
-                    results[sample-1] = sample    
-                    ctr = ctr + 1
-                    print('Ctr: ' + str(ctr) + ' Sample: ' + str(results[sample-1]))
-                    json_string = {'Seed': str(light), 'Counter': str(ctr), 'Sample': str(sample)}
-                    send_data(client,json_string)
-                    break
+            if BINGO == False:
+                while True:
+                    # generate a new random number
+                    r = random.getrandbits(N)
+                    sample = int(r * pow(2,N-1)/90)
 
-            if ctr == 90:
-                Bingo = True
-##################### FILL IN ##################### 
-            # pass it forward to the network
-            # wait for some time to avoid same package sent multiplie times
-            time.sleep(1)
-#################################################### 
+                    # if ((results[sample] != 0) || (sample == 0)):
+                    if sample == 0 or sample > 90:
+                        #ignore reading
+                        print(str(sample) + ' is ignored. Generating another number')
+                    elif results[sample-1] != 0: 
+                        print('Number ' + str(sample) + ' spotted again. Generating another number') # generate another random number
+                    else:
+                        results[sample-1] = sample    
+                        ctr = ctr + 1
+                        json_string = {'Seed': str(light), 'Counter': str(ctr), 'Sample': str(sample)}
+                        send_data(client,json_string)
+                        json_string= sample
+                        send_to_app(client,json_string)
+                        break
+
+                if ctr == 90:
+                    BINGO = True
+
+                time.sleep(1)
+    print('Game has ended. Press again to start a new one')
+
 def setupClient():
     client = MQTTClient(machine.unique_id(), '192.168.0.10') #New MQTT instance
     client.connect()
-    # client.set_callback(callback_function()) #Callback function for reading from MQTT
-    # client.subscribe("esys/cringo/results")
+    client.set_callback(callback_function) #Callback function for reading from MQTT
+    client.subscribe("esys/cringo/samples/subscribe")
 
     return client
 
@@ -99,9 +103,23 @@ def initNetwork():
         pass
     print('The connection was successfull')
 
+def callback_function(topic, data):
+    received_dict = json.loads(data)
+
+    if topic == b'esys/cringo/samples/subscribe':
+        if received_dict['bingo'] == "1":
+            # print(topic, ':', data)
+            global BINGO
+            BINGO = True
+            print('BINGO = ', BINGO)
+
 def send_data(client,json_string): 
     data= json.dumps(json_string)
-    client.publish('esys/cringo/samples', bytes(data, 'utf-8'))#send  data to broker
+    client.publish('esys/cringo/samples/server', bytes(data, 'utf-8'))#send  data to broker
+
+def send_to_app(client,json_string):
+    data= json.dumps(json_string)
+    client.publish('esys/cringo/samples/publish', bytes(data, 'utf-8'))#send  data to broker
 
 def results_init():
     lst = []
